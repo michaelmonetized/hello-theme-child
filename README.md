0a80c7534aa2e69f0636ff49bcba1040a6608f04
Michael Monetized <michaelmonetized@gmail.com>
2024-06-04 22:49:07 -0400

    ready to launch

---
+++ README.md
@@ -0,0 +1,11 @@
+# Jennings Custom Homes - Custom Hello Elementor Child Theme
+
+[ ] Setup Repo
+
+[ ] Customize functions.php
+
+[ ] Switch to theme
+
+[ ] Run Through Customizer
+
+[ ] Run Through Elementor Site Settings
--- functions.php
+++ functions.php
@@ -2,26 +2,21 @@
 /**
  * Theme functions and definitions.
  *
- * For additional information on potential customization options,
- * read the developers' documentation:
- *
- * https://developers.elementor.com/docs/hello-elementor-theme/
- *
- * @package HelloElementorChild
+ * @package JCH
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.0.0' );
+define( 'JCH_VERSION', '2.0.0' );
 
 /**
  * Load child theme scripts & styles.
  *
  * @return void
  */
-function hello_elementor_child_scripts_styles() {
+function jch_scripts_styles() {
 
 	wp_enqueue_style(
 		'hello-elementor-child-style',
@@ -29,8 +24,8 @@ function hello_elementor_child_scripts_styles() {
 		[
 			'hello-elementor-theme-style',
 		],
-		HELLO_ELEMENTOR_CHILD_VERSION
+		JCH_VERSION
 	);
 
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_scripts_styles', 20 );
+add_action( 'wp_enqueue_scripts', 'jch_scripts_styles', 20 );
